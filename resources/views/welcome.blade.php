3544cfb798bc1ea567a0c32f7571208e4a5bcbdb
axel <axel.bouillart@gmail.com>
2017-02-22 16:52:56 +0100

    update graphique

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,15 +6,15 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
 
-    <title>Pic Resizer</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.9/semantic.min.css">
-    <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
+	<title>Pic Resizer</title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.9/semantic.min.css">
+	<link rel="stylesheet" href="{{ mix('/css/app.css') }}">
 
 </head>
 <body>
 
 
-	<div class="ui  top fixed menu">
+	<div class="ui top fixed menu">
 
 		<div class="item">
 			<h1 class="header">Pics Resizer</h1>
@@ -22,20 +22,9 @@
 
 	</div>
 
-	<br>
-	<br>
-	<br>
-	<br>
-	<br>
+	<img class="ui  rounded  massive bordered image" src="https://unsplash.it/1920/1080?image=1067">
 
-		<img class="ui centered rounded  massive bordered image" src="https://unsplash.it/1920/1080?image=1067">
-
-
-	<br>
-	<br>
-	<br>
-
-	<div class="ui two column centered grid">
+	<div class="inputCopyPosition"> 
 		<div class="ui  huge action input">
 			<input type="text" value="http://ww.short.url/c0opq">
 			<button class="ui teal right labeled icon button">
@@ -45,6 +34,26 @@
 		</div>
 	</div>
 
+	<div class="ui  right fixed vertical menu">
+
+		<div class="ui centered grid">
+
+			<div class="formul">
+				<form action="" method="post" class="ui form">
+					{{csrf_field()}}
+					<div class="field">
+						<label for="largeur">Largeur</label>
+						<input type="text" name="largeur" placeholder="Largeur">
+					</div>
+					<div class="field">
+						<label for="longueur">Longueur</label>
+						<input type="text" name="longueur" placeholder="Longueur">
+					</div>
+					<button type="submit" class="ui teal button">Valider</button>
+				</form>
+			</div>
+		</div>
+	</div>
 
 
 
@@ -54,6 +63,8 @@
 
 
 
-<script src="{{ mix('/js/app.js') }}"></script>
+
+
+	<script src="{{ mix('/js/app.js') }}"></script>
 </body>
 </html>
